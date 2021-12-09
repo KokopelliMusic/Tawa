b0a273b3f5e363d61ec4d4c3873fb8e2b2f7e6d7
Nierot <git@nierot.com>
2021-12-09 19:35:27 +0100

    🗃️ Add GQL scheme

--- src/graphql.ts
+++ src/graphql.ts
@@ -2,17 +2,61 @@ import { gql } from "apollo-server-core";
 
 export const typeDefs = gql`
     type Query {
-        test: [Test!]!
     }
 
-    type Test {
-        id : Int!
+    enum SongType {
+        SPOTIFY,
+        YOUTUBE
+    }
+
+    type Session {
+        id: String!
+        user: User!
+        playlist: Playlist!
+    }
+
+    type User {
+        id: Int!
+        createdAt: String!
+        updatedAt: String!
+
+        username: String!
+        auth0Id: String!
+    }
+
+    type Playlist {
+        id: Int!
+        createdAt: Date!
+        updatedAt: Date!
+
+        user: User!
         name: String!
+        songs: [Song]!
+
+    }
+
+    type Song {
+        id: Int!
+        createdAt: Date!
+        updatedAt: Date!
+
+        title: String!
+        playCount: Int!
+        addedBy: User!
+        playlist: Playlist!
+        songType: SongType!
+        platformId: String!
+
+        # Only Spotify has these properties
+        artist: String
+        length: Int
+        album: String
+        cover: String
     }
 `
 
 export const resolvers = {
     Query: {
-        test: () => [{ id: 1, name: "test" }]
+        // test: () => [{ id: 1, name: 'test' }]
     }
-};
+}
