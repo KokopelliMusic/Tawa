ef0cade614b0f1cab338dbec31688ded7ee64726
Nierot <git@nierot.com>
2021-12-11 09:45:10 +0100

    🏗️ Add scaffolding for util

---
+++ src/util.ts
@@ -0,0 +1,5 @@
+
+// Returns a random integer between x (inclusive) and y (inclusive)
+export const randomIntBetweenXandY = (x: number, y: number): number => {
+  throw new Error('TODO - util.randomIntBetweenXandY')  
+}
